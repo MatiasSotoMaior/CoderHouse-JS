f98d1cf305a3ed6275416d547a536c1899f1d0e9
MatiasSotoMaior <89552410+MatiasSotoMaior@users.noreply.github.com>
2021-11-25 23:46:55 -0300

    Add files via upload

---
+++ clase8-DOM/js/dom.js
@@ -0,0 +1,13 @@
+console.log(document);
+
+console.log(document.head);
+
+console.log(document.body);
+
+let div = document.getElementById("box")
+
+console.log(div)
+
+let lol = document.getElementsByClassName("lol")
+console.log(lol)
+console.log(lol[1])
